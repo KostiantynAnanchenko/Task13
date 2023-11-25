33520ab9fedf95772ec33592a11a397342cbc3da
KostiantynAnanchenko <itemswap@gmail.com>
2023-11-25 23:11:35 +0100

    1

--- src/main/java/Task13/JsonPlaceholderApi.java
+++ src/main/java/Task13/JsonPlaceholderApi.java
@@ -1,2 +1,125 @@
-package Task13;public class JsonPlaceholderApi {
+package Task13;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class JsonPlaceholderApi {
+
+    private static final String BASE_URL = "https://jsonplaceholder.typicode.com";
+
+    private static String sendRequest(String endpoint, String requestMethod, String requestBody) {
+        try {
+            URL url = new URL(BASE_URL + endpoint);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod(requestMethod);
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+
+            if (requestBody != null && !requestBody.isEmpty()) {
+                try (OutputStream os = connection.getOutputStream();
+                     BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"))) {
+                    writer.write(requestBody);
+                    writer.flush();
+                }
+            }
+
+            StringBuilder response = new StringBuilder();
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String line;
+                while ((line = in.readLine()) != null) {
+                    response.append(line);
+                }
+            }
+
+            connection.disconnect();
+            return response.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    // ---Task 1---
+    public static String createUser(String requestBody) {
+        return sendRequest("/users", "POST", requestBody);
+    }
+
+    public static String updateUser(int userId, String updatedUserJson) throws IOException {
+        return sendRequest("/users/" + userId, "PUT", updatedUserJson);
+    }
+
+    public static int deleteUser(int userId) throws IOException {
+        URL url = new URL(BASE_URL + "/users/" + userId);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("DELETE");
+        return connection.getResponseCode();
+    }
+
+    public static String getAllUsers() {
+        return sendRequest("/users", "GET", null);
+    }
+
+    public static String getUserById(int userId) {
+        return sendRequest("/users/" + userId, "GET", null);
+    }
+
+    public static String getUserByUsername(String username) {
+        return sendRequest("/users?username=" + username, "GET", null);
+    }
+
+    // ---Task 2---
+    public static void getCommentsForLatestPostOfUser(int userId) {
+        String postsEndpoint = "/users/" + userId + "/posts";
+        String postsResponse = sendRequest(postsEndpoint, "GET", null);
+
+        int latestPostId = -1;
+        if (postsResponse != null) {
+            JSONArray postsArray = new JSONArray(postsResponse);
+            if (postsArray.length() > 0) {
+                JSONObject latestPost = postsArray.getJSONObject(postsArray.length() - 1);
+                latestPostId = latestPost.getInt("id");
+            }
+        }
+        if (latestPostId != -1) {
+            String commentsEndpoint = "/posts/" + latestPostId + "/comments";
+            String commentsResponse = sendRequest(commentsEndpoint, "GET", null);
+
+
+            if (commentsResponse != null) {
+                String fileName = "user-" + userId + "-post-" + latestPostId + "-comments.json";
+                try (PrintWriter writer = new PrintWriter(fileName)) {
+                    writer.write(commentsResponse);
+                    System.out.println("Comments saved to file: " + fileName);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // ---Task 3---
+    public static String getOpenTasksForUser(int userId) {
+        StringBuilder result = new StringBuilder();
+        String tasksEndpoint = "/users/" + userId + "/todos";
+        String tasksResponse = sendRequest(tasksEndpoint, "GET", null);
+
+        if (tasksResponse != null) {
+            JSONArray tasksArray = new JSONArray(tasksResponse);
+            result.append("Open tasks for user ").append(userId).append(" (not completed):\n");
+
+            for (int i = 0; i < tasksArray.length(); i++) {
+                JSONObject task = tasksArray.getJSONObject(i);
+                if (!task.getBoolean("completed")) {
+                    result.append("Task ID: ").append(task.getInt("id")).append(", Title: ").append(task.getString("title")).append("\n");
+                }
+            }
+        }
+
+        return result.toString();
+    }
+
+
 }
--- src/main/java/Task13/Main.java
+++ src/main/java/Task13/Main.java
@@ -1,22 +1,57 @@
-package Task11;
+package Task13;
 
-import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.nio.file.Files;
+
+import static Task13.JsonPlaceholderApi.*;
 
 public class Main {
 
-    private static final String TEST_URL= "https://jsonplaceholder.typicode.com/users";
 
     public static void main(String[] args) throws IOException {
-        URL url=new URL(TEST_URL);
+
+        System.out.println("TASK 1");
+
+        // Task 1.1 Create new user
+        String newUserData = "{\"ExanpleName\": \"John Examole\", \"username\": \"example\", \"email\": \"john@example.com\"}";
+        String createdUserResponse = createUser(newUserData);
+        System.out.println("Created user response: " + createdUserResponse);
+
+        // Task 1.2 Update user by id.
+        int userIdToUpdate = 1;
+        String updatedUserJson = "{\"name\": \"Updated Name\", \"username\": \"updatedusername\", \"email\": \"updated@example.com\"}";
+        String updatedUser = updateUser(userIdToUpdate, updatedUserJson);
+        System.out.println("Updated user: " + updatedUser);
+
+        // Task 1.3 Delete user by id
+        int userIdToDelete = 3;
+        int deletionResponse = deleteUser(userIdToDelete);
+        System.out.println("Deletion response code: " + deletionResponse);
+
+        // Task 1.4  Getting all users information
+        String allUsersInfo = getAllUsers();
+        System.out.println("All users: " + allUsersInfo);
+
+        // Task 1.5 Getting information about user by id
+        int userIdInfo = 1;
+        String userInfoById = getUserById(userIdInfo);
+        System.out.println("User info by id " + userIdInfo + ": " + userInfoById);
+
+        // Task 1.6 Getting information about user by username
+        String username = "Bret";
+        String userInfoByUsername = getUserByUsername(username);
+        System.out.println("User info by username " + username + ": " + userInfoByUsername);
 
 
+        // Task 2 Getting comments and writing to a file
+        System.out.println("\nTASK 2");
+        getCommentsForLatestPostOfUser(3);
+
+
+        // Task 3: Getting open tasks for a user
+        System.out.println("\nTASK 3");
+        int userId = 1;
+        String openTasksForUser = getOpenTasksForUser(userId);
+        System.out.println("Open tasks for user " + userId + ": " + openTasksForUser);
     }
+
 }
