c3b73121ecf96d056f2f477013346b20f7ae59b7
KostiantynAnanchenko <itemswap@gmail.com>
2023-11-25 23:03:24 +0100

    Initial commit

---
+++ src/main/java/Task13/Main.java
@@ -0,0 +1,22 @@
+package Task11;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+
+public class Main {
+
+    private static final String TEST_URL= "https://jsonplaceholder.typicode.com/users";
+
+    public static void main(String[] args) throws IOException {
+        URL url=new URL(TEST_URL);
+
+
+    }
+}
